56cabb2fae78c19c093eac4f29b7f71d4a9010d2
aitor614 <110167746+aitor614@users.noreply.github.com>
2023-01-12 11:32:49 +0100

    Add files via upload

---
+++ Fase_2_Ejercicio_1_B/AA2(S3)_Sanchez_Cervantes_Aitor_Bucle_For.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//Likamos las librerías
+#include <stdio.h>
+#include <stdbool.h>
+
+//Algoritmo Digitos
+
+int main(void) {
+	// Declaracion de variables
+
+	int n, numero, contador, numeroProvisional, numero1, numeroCentena;
+	bool digitos;
+
+	contador = 0;
+
+
+	for (n = 1; n < 1000; n++) {
+
+		digitos = false;
+
+		if (n < 10) {
+			if (n == 9) {
+				digitos = true;
+			}
+		}
+		else
+			if (n < 100) {
+				numero = ((n / 10) + (n % 10));
+				if (numero > 9) {
+					numero = ((numero / 10) + (numero % 10));
+				}
+				if (numero == 9) {
+					digitos = true;
+				}
+			}
+			else
+				if (n < 1000) {
+					numeroProvisional = (n / 10);
+					numero1 = (n % 10);
+					numeroCentena = ((numeroProvisional / 10) + (numeroProvisional % 10) + (numero1));
+					if (numeroCentena > 9) {
+						numeroCentena = ((numeroCentena / 10) + (numeroCentena % 10));
+					}
+					if (numeroCentena == 9) {
+						digitos = true;
+					}
+				}
+		if (digitos == true) {
+			contador++;
+		}
+
+	}
+	printf("Multiplos de 9: %d\n", contador);
+	return 0;
+}
